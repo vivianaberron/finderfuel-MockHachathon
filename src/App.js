00a20291b4ce7a3843cb4a6d75c2bbdb0ab91992
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2019-11-22 12:28:06 -0600

    save the call to api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Otherview from './views/otherview';
 import LandingView from './views/LandingView';
 import Welcome from './views/WelcomeView';
 import PemexView from './views/PemexView';
+import NameList from './views/NameList';
 
 import './App.css';
 
@@ -15,6 +16,7 @@ class App extends Component {
            <Switch>
               <Route exact path='/' component={Welcome} />
               <Route exact path='/landing' component={LandingView} />
+              <Route exact path='/list' component={NameList} />
               <Route exact path='/pemex' component={PemexView} />
               <Route exact path='/otherview' component={Otherview} />
            </Switch>
---
+++ src/components/map/ApiList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class NameList extends React.Component {
+    state = {
+        data: []
+    }
+    componentDidMount(){
+       axios.get('https://cors-anywhere.herokuapp.com/https://api-gas-stations-mex.herokuapp.com/gasstations')
+           .then(response => {
+               console.log(response)
+               this.setState({
+                   data: response.data
+               });
+           }).catch(error => {console.log(error);});
+       }
+       render(){
+           return(
+               <ul>
+                   {this.state.data.map(info => <li key={info.index}>{info.location.x} </li>)}
+               </ul>
+           )
+       }
+}
